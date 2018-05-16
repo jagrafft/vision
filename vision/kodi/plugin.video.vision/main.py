7109306e8d4ddb235c6960a520a4239502ccad2b
jason a. grafft <graf0096@umn.edu>
2018-05-16 14:45:38 -0500

    Kodi AddOn for static content functional

--- vision/kodi/plugin.video.vision/main.py
+++ vision/kodi/plugin.video.vision/main.py
@@ -13,22 +13,29 @@ import xbmcplugin
 _url = sys.argv[0]
 _handle = int(sys.argv[1])
 
-FEEDS = {
+host = "160.94.183.61"
+
+VIDEOS = {
             'A576': [
-                {'name': 'Patient Left','addr': 'rtsp://192.168.1.201:554/axis-media/media.amp?profile=Quality'},
-                {'name': 'Patient Foot','addr': 'rtsp://192.168.1.202:554/axis-media/media.amp?profile=Quality'}
+                {'name': 'Patient Left','video': 'rtsp://192.168.1.201:554/axis-media/media.amp?profile=Quality', 'thumb': './resources/icon.png', 'genre': 'RTSP'},
+                {'name': 'Patient Foot','video': 'rtsp://192.168.1.202:554/axis-media/media.amp?profile=Quality', 'thumb': './resources/icon.png', 'genre': 'RTSP'}
             ],
             'A574': [
-                {'name': 'Overhead','addr': 'rtsp://192.168.1.203:554/axis-media/media.amp?profile=Quality'}
+                {'name': 'Overhead','video': 'rtsp://192.168.1.203:554/axis-media/media.amp?profile=Quality', 'thumb': './resources/icon.png', 'genre': 'RTSP'}
             ],
-            'Anesthesia RTSP': [
-                {'name': 'Beaumont (mp4)','addr': 'rtsp://160.94.183.61:8555/test'},
-                {'name': 'SimMan (mp4)','addr': 'rtsp://160.94.183.61:8556/test'},
-                {'name': 'Traffic (mp4)','addr': 'rtsp://160.94.183.61:8557/test'}
-            ],
-            'Anesthesia DASH': [
-                {'name': '001','addr': 'http://160.94.183.61/dash/mp4/001/manifest.mpd'},
-                {'name': '002','addr': 'http://160.94.183.61/dash/mp4/002/manifest.mpd'}
+            # 'GST-RTSP (Anesthesia)': [
+            #     {'name': 'Beaumont (mp4)','video': 'rtsp://{}:8555/test'.format(host), 'thumb': './resources/icon.png', 'genre': 'RTSP'},
+            #     {'name': 'SimMan (mp4)','video': 'rtsp://{}:8556/test'.format(host), 'thumb': './resources/icon.png', 'genre': 'RTSP'},
+            #     {'name': 'Traffic (mp4)','video': 'rtsp://{}:8557/test'.format(host), 'thumb': './resources/icon.png', 'genre': 'RTSP'}
+            # ],
+            # 'DASH (Anesthesia)': [
+            #     {'name': '001','video': 'http://{}/dash/mp4/001/manifest.mpd'.format(host), 'thumb': './resources/icon.png', 'genre': 'DASH'},
+            #     {'name': '002','video': 'http://{}/dash/mp4/002/manifest.mpd'.format(host), 'thumb': './resources/icon.png', 'genre': 'DASH'}
+            # ],
+            'HLS (Anesthesia)': [
+                {'name': '001','video': 'http://{}/hls/mp4/001/playlist.m3u8'.format(host), 'thumb': './resources/icon.png', 'genre': 'HLS'},
+                {'name': '002','video': 'http://{}/hls/mp4/002/playlist.m3u8'.format(host), 'thumb': './resources/icon.png', 'genre': 'HLS'},
+                {'name': '003','video': 'http://{}/hls/mp4/003/playlist.m3u8'.format(host), 'thumb': './resources/icon.png', 'genre': 'HLS'}
             ]
         }
 
@@ -36,58 +43,70 @@ def get_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs))
 
 def get_categories():
-    return FEEDS.iterkeys()
+    return VIDEOS.iterkeys()
 
-def get_feeds(category):
-    return FEEDS[category]
+def get_videos(category):
+    return VIDEOS[category]
 
 def list_categories():
-    xbmcplugin.setPluginCategory(_handle, 'Overhead Cameras')
-    xbmcplugin.setContent(_handle, 'feeds')
+    xbmcplugin.setPluginCategory(_handle, 'vision Recordings')
+    xbmcplugin.setContent(_handle, 'videos')
     categories = get_categories()
-
+    
     for category in categories:
-        item = xbmcgui.ListItem(label=category)
-        item.setInfo('feed', {'title': category, 'mediatype': 'video'})
+        list_item = xbmcgui.ListItem(label=category)
+        # list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
+                        #   'icon': VIDEOS[category][0]['thumb'],
+                        #   'fanart': VIDEOS[category][0]['thumb']})
+        list_item.setInfo('video', {'title': category,
+                                    'genre': category,
+                                    'mediatype': 'video'})
         url = get_url(action='listing', category=category)
         is_folder = True
-        xbmcplugin.addDirectoryItem(_handle, url, item, is_folder)
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
     
     xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     xbmcplugin.endOfDirectory(_handle)
 
-def list_feeds(category):
+
+def list_videos(category):
     xbmcplugin.setPluginCategory(_handle, category)
-    xbmcplugin.setContent(_handle, 'feeds')
-    feeds = get_feeds(category)
-
-    for feed in feeds:
-        item = xbmcgui.ListItem(label=feed['name'])
-        item.setInfo('feed', {'title': feed['name'], 'mediatype': 'video'})
-        item.setProperty('IsPlayable', 'true')
-        url = get_url(action='play', video=feed['addr'])
+    xbmcplugin.setContent(_handle, 'videos')
+    videos = get_videos(category)
+    
+    for video in videos:
+        list_item = xbmcgui.ListItem(label=video['name'])
+        list_item.setInfo('video', {'title': video['name'],
+                                    'genre': video['genre'],
+                                    'mediatype': 'video'})
+        # list_item.setArt({'thumb': video['thumb'], 'icon': video['thumb'], 'fanart': video['thumb']})
+        list_item.setProperty('IsPlayable', 'true')
+        url = get_url(action='play', video=video['video'])
         is_folder = False
-        xbmcplugin.addDirectoryItem(_handle, url, item, is_folder)
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
     
     xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     xbmcplugin.endOfDirectory(_handle)
 
-def play_feed(path):
-    item = xbmcgui.ListItem(path=path)
-    xbmcplugin.setResolvedUrl(_handle, True, listitem=item)
 
-def router(paramstr):
-    params = dict(parse_qsl(paramstr))
+def play_video(path):
+    play_item = xbmcgui.ListItem(path=path)
+    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+
+
+def router(paramstring):
+    params = dict(parse_qsl(paramstring))
 
     if params:
         if params['action'] == 'listing':
-            list_feeds(params['category'])
+            list_videos(params['category'])
         elif params['action'] == 'play':
-            play_feed(params['video'])
+            play_video(params['video'])
         else:
-            raise ValueError('paramstr invalid: {0}'.format(paramstr))
+            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
     else:
         list_categories()
+
 
 if __name__ == '__main__':
     router(sys.argv[2][1:])
