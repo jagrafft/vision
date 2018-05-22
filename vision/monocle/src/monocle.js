5360ac3742a63e4e07cee2a49ef7cf3f3271352b
jason a. grafft <jgrafft@gmail.com>
2018-05-22 16:37:20 -0500

    Cleanup edits.

--- vision/monocle/src/monocle.js
+++ vision/monocle/src/monocle.js
@@ -4,9 +4,9 @@ import { run } from "@cycle/run";
 function main(sources) {
   const input_ = sources.DOM.select(".field").events("input")
 
-  const name$ = input_.map(ev => ev.target.value).startWith("")
+  const name_ = input_.map(ev => ev.target.value).startWith("")
 
-  const vdom_ = name$.map(name =>
+  const vdom_ = name_.map(name =>
     div([
       label("w00t:"),
       input(".field", {attrs: {type: "text"}}),
