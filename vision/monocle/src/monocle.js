04f2bcd3e1d98338d9760f459b23084d02e2782e
jason a. grafft <graf0096@umn.edu>
2018-05-25 16:17:48 -0500

    Started list selection for Monocle.

--- vision/monocle/src/monocle.js
+++ vision/monocle/src/monocle.js
@@ -1,46 +1,72 @@
-// import { div, h1, hr, input, label, makeDOMDriver } from "@cycle/dom";
-import { h1, div, input, p, makeDOMDriver } from "@cycle/dom";
-import { run } from "@cycle/run";
-// import xstream from "xstream";
+/*jslint es6*/
+import xs from "xstream";
+import isolate from "@cycle/isolate";
+import {div, span, input, label, makeDOMDriver} from "@cycle/dom";
+import {run} from "@cycle/run";
 
-// const tempData = Object.freeze([
-//   { label: "w00t", id: 1 },
-//   { label: "s00t", id: 2 },
-//   { label: "r00t", id: 3 },
-//   { label: "b00t", id: 4 },
-//   { label: "p00t", id: 5 }
-// ]);
+function List(sources) {
+    const props = Object.freeze([
+        {label: "w00t", id: 1},
+        {label: "s00t", id: 2},
+        {label: "r00t", id: 3},
+        {label: "b00t", id: 4},
+        {label: "p00t", id: 5}
+    ]);
 
-function main(sources) {
-  const sinks = {
-    DOM: sources.DOM.select('input').events('click')
-      .map(ev => ev.target.checked)
-      .startWith(false)
-      .map(toggled =>
-        div([
-          input({attrs: {type: 'checkbox'}}), 'Toggle me',
-          p(toggled ? 'ON' : 'off')
-        ])
-      )
-  };
-  return sinks;
+    function isolateList (props) {
+        return props.reduce(function (prev, prop) {
+            return prev.concat(isolate(ListItem)({Props: xs.of(prop), DOM: sources.DOM}).DOM);
+        }, []);
+    }
+
+    const vdom_ = xs.combine.apply(null, isolateList(props))
+        .map((x) => div(".list", x));
+
+    return {
+        DOM: vdom_
+    };
 }
 
-// function main(sources) {
-//   const input_ = sources.DOM.select(".field").events("input")
+function ListItem(sources) {
+    const domSource = sources.DOM;
+    const props_ = sources.Props;
 
-//   const name_ = input_.map(ev => ev.target.value).startWith("")
+    const srcIds_ = domSource
+        .select(".src")
+        .events("change")
+        .map((ev) => ({id: ev.target.id, checked: ev.target.checked}));
 
-//   const vdom_ = name_.map(name =>
-//     div([
-//       label("w00t:"),
-//       input(".field", {attrs: {type: "text"}}),
-//       hr(),
-//       h1("Høla, " + name),
-//     ])
-//   )
+    srcIds_.addListener({
+        next: function handleNextEvent(event) {
+            console.log(`event: {id: ${event.id}, checked: ${event.checked}}`);
+        },
+        error: function handleError(error) {
+            console.error(`error: ${error}`);
+        },
+        complete: function handleCompleted() {
+            console.log(`completed`);
+        }
+    });
 
-//   return { DOM: vdom_ }
-// }
+    const state$ = props_
+        .map(props => srcIds_
+            .map((val) => ({id: props.id, label: props.label}))
+            .startWith(props)
+        )
+        .flatten();
 
-run(main, { DOM: makeDOMDriver("#app") })
+    const vdom_ = state$
+        .map(state => div(".list-item", [
+            input(".src", {
+                attrs: {type: "checkbox", id: state.id}
+            }),
+            label({attrs: {for: state.id}}, state.label)
+        ])
+    );
+
+    return {
+        DOM: vdom_
+    };
+}
+
+run(List, {DOM: makeDOMDriver("#app")});
