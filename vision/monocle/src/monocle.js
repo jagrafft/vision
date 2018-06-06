3bbcff9902b2d5212823ac96bb11e44079170389
jason a. grafft <graf0096@umn.edu>
2018-06-06 14:44:17 -0500

    Monocle: flag selection, add/rem from localStorage. Added licenses.

--- vision/monocle/src/monocle.js
+++ vision/monocle/src/monocle.js
@@ -1,7 +1,7 @@
 /*jslint es6*/
 import xs from "xstream";
 import isolate from "@cycle/isolate";
-import {div, span, input, label, makeDOMDriver} from "@cycle/dom";
+import {div, span, makeDOMDriver} from "@cycle/dom";
 import {run} from "@cycle/run";
 
 function List(sources) {
@@ -30,15 +30,16 @@ function List(sources) {
 function ListItem(sources) {
     const domSource = sources.DOM;
     const props_ = sources.Props;
+    let selectedIds = [];
 
     const srcIds_ = domSource
         .select(".src")
-        .events("change")
-        .map((ev) => ({id: ev.target.id, checked: ev.target.checked}));
+        .events("click")
+        .map((ev) => ({id: ev.target.id}));
 
     srcIds_.addListener({
         next: function handleNextEvent(event) {
-            console.log(`event: {id: ${event.id}, checked: ${event.checked}}`);
+            localStoreTransact(event);
         },
         error: function handleError(error) {
             console.error(`error: ${error}`);
@@ -49,24 +50,30 @@ function ListItem(sources) {
     });
 
     const state$ = props_
-        .map(props => srcIds_
-            .map((val) => ({id: props.id, label: props.label}))
+        .map((props) => srcIds_
+            .map(() => ({id: props.id, label: props.label}))
             .startWith(props)
         )
         .flatten();
 
     const vdom_ = state$
-        .map(state => div(".list-item", [
-            input(".src", {
-                attrs: {type: "checkbox", id: state.id}
-            }),
-            label({attrs: {for: state.id}}, state.label)
-        ])
-    );
+        .map((state) => div(".list-item", [
+            span(".src", {
+                attrs: {id: state.id}
+            }, state.label),
+            span(localStoreLookup(`src${state.id}`) ? "*" : "")
+        ]));
 
     return {
         DOM: vdom_
     };
+}
+
+const localStoreLookup = (id) => localStorage.getItem(id) === null ? false : true;
+
+function localStoreTransact(obj) {
+    const key = `src${obj.id}`;
+    localStorage.getItem(key) === null ? localStorage.setItem(key, obj.id) : localStorage.removeItem(key);
 }
 
 run(List, {DOM: makeDOMDriver("#app")});
--- vision/retina/project/Dependencies.scala
+++ vision/retina/project/Dependencies.scala
@@ -1,40 +1,43 @@
 import sbt._
 
 object Dependencies {
-  val circeVersion = "0.9.3"
-  val cirisVersion = "0.9.2"
-  val doobieVersion = "0.5.2"
-  val http4sVersion = "0.18.11"
-  val scalaTestVersion = "3.0.5"
+    val catsVersion = "1.0.1"
+    val circeVersion = "0.9.3"
+    val cirisVersion = "0.9.2"
+    val doobieVersion = "0.5.2"
+    val http4sVersion = "0.18.11"
+    val scalaTestVersion = "3.0.5"
 
-  lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
-  lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
-  lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
-  
-  lazy val cirisCore = "is.cir" %% "ciris-core" % cirisVersion
+    lazy val catsCore = "org.typelevel" %% "cats-core" % catsVersion
 
-  lazy val doobieCore = "org.tpolecat" %% "doobie-core" % doobieVersion
-  lazy val doobieTest = "org.tpolecat" %% "doobie-scalatest" % doobieVersion
-  
-  lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % http4sVersion
-  lazy val http4sServer = "org.http4s" %% "http4s-blaze-server" % http4sVersion
-  lazy val http4sClient = "org.http4s" %% "http4s-blaze-client" % http4sVersion
+    lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
+    lazy val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
+    lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
+    lazy val cirisCore = "is.cir" %% "ciris-core" % cirisVersion
 
-  lazy val commonDependencies = Seq(
-      cirisCore,
-      scalaTest % Test
-  )
+    lazy val doobieCore = "org.tpolecat" %% "doobie-core" % doobieVersion
+    lazy val doobieTest = "org.tpolecat" %% "doobie-scalatest" % doobieVersion
 
-  lazy val http = Seq(
-      circeCore,
-      circeGeneric,
-      circeParser,
-      doobieCore,
-      doobieTest % Test,
-      http4sDsl,
-      http4sServer,
-      http4sClient
+    lazy val http4sDsl = "org.http4s" %% "http4s-dsl" % http4sVersion
+    lazy val http4sServer = "org.http4s" %% "http4s-blaze-server" % http4sVersion
+    lazy val http4sClient = "org.http4s" %% "http4s-blaze-client" % http4sVersion
+
+    lazy val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
+
+    lazy val commonDependencies = Seq(
+        catsCore,
+        cirisCore,
+        scalaTest % Test
+    )
+
+    lazy val http = Seq(
+        circeGeneric,
+        circeParser,
+        doobieCore,
+        doobieTest % Test,
+        http4sDsl,
+        http4sServer,
+        http4sClient
   )
 }
